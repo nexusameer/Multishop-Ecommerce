c615d25e1fecc8fafd69c0b9e0a7bd421887c1d6
nexusameer <ameerk10fw@gmail.com>
2024-07-10 20:25:40 +0500

    project setup (apps created)

--- Multishop/settings.py
+++ Multishop/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'user',
+    'products',
+    'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
